4bfe51bed56e37cab4a49218c6ad1ac96759d657
Evan Roberts <eroberts1998@icloud.com>
2025-09-09 08:36:34 -0400

    getFavoriteArtists written, not tested

---
+++ controllers/shared/favoriteArtistsController.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require("sequelize");
+const db = require("./../../server");
+const AppError = require("../../utils/appError");
+const catchAsync = require("./../../utils/catchAsync");
+const FavoriteArtists = require("./../../models/shared/FavoriteArtists");
+
+exports.getFavoriteArtists = catchAsync(async (req, res, next) => {
+  const usersFavoriteArtists = await FavoriteArtists.findAll({
+    where: { userId: req.user.id },
+    order: [["favoritedAt", "DESC"]],
+  });
+
+  res.status(200).json({ status: "success", data: usersFavoriteArtists });
+});
