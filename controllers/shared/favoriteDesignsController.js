5c9c130d15c7449ef2d75823810bed8c2ad84c6e
Evan Roberts <eroberts1998@icloud.com>
2025-09-11 16:14:54 -0400

    getFavoriteDesigns and getPublicUsersFavoriteDesigns written, non tested

---
+++ controllers/shared/favoriteDesignsController.js
@@ -0,0 +1,44 @@
+const { Sequelize } = require("sequelize");
+const db = require("./../../server");
+const AppError = require("../../utils/appError");
+const catchAsync = require("./../../utils/catchAsync");
+const Users = require("./../../models/shared/Users");
+const FavoriteDesigns = require("./../../models/shared/FavoriteDesigns");
+const TattooDesigns = require("./../../models/artists/TattooDesigns");
+
+exports.getFavoriteDesigns = catchAsync(async (req, res, next) => {
+  const usersFavoriteDesigns = await FavoriteDesigns.findAll({
+    where: { userId: req.user.id },
+    order: [["favoritedAt", "DESC"]],
+  });
+
+  res.status(200).json({
+    status: "success",
+    results: usersFavoriteDesigns.length,
+    data: { usersFavoriteDesigns },
+  });
+});
+
+exports.getPublicUsersFavoriteDesigns = catchAsync(async (req, res, next) => {
+  const user = await Users.findOne({
+    where: { publicId: req.params.publicId },
+    attributes: ["id", "publicId", "displayName"],
+  });
+
+  const favoriteDesigns = await FavoriteDesigns.findAll({
+    where: { userId: user.id },
+    include: [
+      {
+        model: TattooDesigns,
+        as: "design",
+        attributes: ["publicId", "title", "thumbnailUrl"],
+      },
+    ],
+    order: [["favoritedAt", "DESC"]],
+  });
+
+  const usersFavoriteDesigns = favoriteDesigns.map((fav) => fav.design);
+
+  res.status(200).json({ status: "success", data: { usersFavoriteDesigns } });
+});
+
